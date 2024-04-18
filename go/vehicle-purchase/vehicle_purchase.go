2991cc97a7db57de767f1353cbd72256cecd235e
Caleb McClain <caleb.mcclain89@gmail.com>
2024-04-18 21:00:30 +0900

    fun

---
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,29 @@
+package purchase
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	result := " is clearly the better choice.";
+	if option1 < option2 {
+		result = option1 + result
+	} else {
+		result = option2 + result
+	}
+
+	return result
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return originalPrice * 0.80
+	} else if age >= 3 && age < 10 {
+		return originalPrice * 0.70
+	} else {
+		return originalPrice * 0.50
+	}
+}
